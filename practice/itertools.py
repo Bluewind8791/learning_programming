e81ec38599743f9befe9ec6944132c84b127f179
Bluewind <bluewind@kakao.com>
2021-09-07 10:44:10 +0900

    9/7 second question

--- practice/itertools.py
+++ practice/itertools.py
@@ -11,4 +11,11 @@ decimals = [0, 7, 0, 8, 0, 9, 0, 4, 0, 5]
 # ['a', 'b', 'c', 'd', 'e', 1, 0, 1, 0, 1, 0, 7, 0, 8, 0, 9, 0, 4, 0, 5]
 
 
-# imap()
+# combinations(조합, 길이)
+# 각 객채의 조합을 만들어서 반환한다
+
+chars = ["A", "B", "C"]
+
+combination = itertools.combinations(chars, 2)
+print(list(combination))
+# [('A', 'B'), ('A', 'C'), ('B', 'C')]

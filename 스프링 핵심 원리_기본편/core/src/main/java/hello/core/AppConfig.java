c39b88a271b19018bff0306f757f9edb056da043
Bluewind <bluewind@kakao.com>
2023-06-12 13:03:09 +0900

    commit

--- 스프링 핵심 원리_기본편/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심 원리_기본편/core/src/main/java/hello/core/AppConfig.java
@@ -8,27 +8,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
     // MemberRepository 연결
-    private MemoryMemberRepository getMemberRepository() {
+    @Bean
+    public MemoryMemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
     // DiscountPolicy 연결
-    private DiscountPolicy getDiscountPolicy() {
+    @Bean
+    public DiscountPolicy getDiscountPolicy() {
 //        return new FixDiscountPolicy();
         // 정률 할인정책으로 변경
         return new RateDiscountPolicy();
     }
 
     // MemberService DI
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(getMemberRepository());
     }
 
     // OrderService DI
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }

b4f5f77590964071803724320ee6a140abc39177
Bluewind <bluewind@kakao.com>
2023-06-15 12:53:37 +0900

    23-06-15 study commit

--- 스프링 핵심 원리_기본편/core/src/main/java/hello/core/MemberApp.java
+++ 스프링 핵심 원리_기본편/core/src/main/java/hello/core/MemberApp.java
@@ -5,6 +5,7 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MemberApp {
 
--- 스프링 핵심 원리_기본편/core/src/test/java/hello/core/beanfind/AppContextExtendsFindTest.java
+++ 스프링 핵심 원리_기본편/core/src/test/java/hello/core/beanfind/AppContextExtendsFindTest.java
@@ -11,6 +11,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
 class AppContextExtendsFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
@@ -20,6 +24,27 @@ class AppContextExtendsFindTest {
     void findBeanByParentTypeDuplicate() {
 //        DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
         Assertions.assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상있으면 이름으로 조회")
+    void findBeanByParentTypeDuplicateSuccess() {
+        DiscountPolicy bean = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        assertThat(bean).isInstanceOf(DiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회 시, 자식이 둘 이상있으면 특정 하위 타입으로 조회")
+    void findBeanByParentTypeByType() {
+        DiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        assertThat(bean).isInstanceOf(DiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회")
+    void findAllBeanByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
     }
 
     @Configuration

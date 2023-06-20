8c47b1f5bd1d7097b9b0c19339495e91b3ed47eb
Bluewind <bluewind@kakao.com>
2023-06-20 14:12:48 +0900

    study commit

--- 스프링 핵심 원리_기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ 스프링 핵심 원리_기본편/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,8 +2,13 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.format.TextStyle;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,4 +42,15 @@ public class SingletonTest {
         // same == 비교
         // equal .equals 비교
     }
+
+    @Test
+    @DisplayName("싱글톤 컨테이너 테스트")
+    void springContainerTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+
+
 }

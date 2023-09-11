c8198cd4990edd822c3a59aceef2ce0478d42dfa
Bluewind <bluewind@kakao.com>
2023-09-11 12:52:23 +0900

    #6.4 완강

--- nomad_coder/react-for-beginners/src/App.js
+++ nomad_coder/react-for-beginners/src/App.js
@@ -1,12 +1,36 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [counter, setCounter] = useState(0);
+    const onClick = () => setCounter((prev) => prev + 1);
+    // called only one time
+    useEffect(() => {
+       console.log("i run only once");
+    }, []);
+
+    const [keyword, setKeyword] = useState("");
+    const onChange = (event) => setKeyword(event.target.value);
+    // called when keyword changed
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("SEARCHING... ", keyword);
+        }
+    }, [keyword]);
+
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!</h1>
-        <Button text={"Continue"}/>
-    </div>
+            <input
+                type="text"
+                onChange={onChange}
+                value={keyword}
+                placeholder="Search here..."
+            />
+            <h1 className={styles.title}>{counter}</h1>
+            <button onClick={onClick}>Click me!</button>
+        </div>
   );
 }
 

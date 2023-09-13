032950764ce728713a6b6bf4e7b3e23e17ae7415
Bluewind8791 <bluewind@kakao.com>
2023-09-13 22:01:52 +0900

    #7.0, #7.1 To Do list

--- nomad_coder/react-for-beginners/src/App.js
+++ nomad_coder/react-for-beginners/src/App.js
@@ -1,11 +1,37 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import {useState} from "react";
 
+// #7.0, #7.1 To Do list
 function App() {
+
+    const [todo, setTodo] = useState("");
+    function onChange(event) {
+        setTodo(event.target.value);
+    }
+
+    const [todoList, setTodoList] = useState([]);
+    function submit(event) {
+        event.preventDefault();
+        if (todo === "") return;
+        setTodoList((curr) => [todo, ...curr]);
+        setTodo("");
+    }
+
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!</h1>
-        <Button text={"Continue"}/>
+            <h1>My To Do List</h1>
+            <form onSubmit={submit}>
+                <input
+                    type="text"
+                    placeholder="Write your to do..."
+                    value={todo}
+                    onChange={onChange}
+                />
+                <button type="submit">Add</button>
+            </form>
+            <hr/>
+            <ul>
+                {todoList.map((todo, idx) => <li key={idx}>{todo}</li>)}
+            </ul>
     </div>
   );
 }

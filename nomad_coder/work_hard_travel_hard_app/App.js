c548144679bb02f6a4cb0b4f91abbf3623348a14
Bluewind <bluewind@kakao.com>
2023-10-31 18:05:20 +0900

    오류 수정 / 코드 챌린지 1,2 완성

--- nomad_coder/work_hard_travel_hard_app/App.js
+++ nomad_coder/work_hard_travel_hard_app/App.js
@@ -1,5 +1,5 @@
 import {Alert, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 import { StatusBar } from 'expo-status-bar';
 import { Fontisto } from "@expo/vector-icons";
@@ -9,19 +9,38 @@ import { theme } from './colors';
 
 
 const STORAGE_KEY = "@toDos";
+const STORAGE_TAB_KEY = "@tab";
+
+// 수정 기능 추가
+
 
 export default function App() {
+
+  const textInput = useRef();
+
   useEffect(() => {
     loadToDos().then();
+    loadTabInfo();
   }, []);
 
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
-  const travel = () => setWorking(false);
-  const work = () => setWorking(true);
-  const onChangeText = (payload) => setText(payload);
 
+  const travel = () => switchTab(false);
+  const work = () => switchTab(true);
+  function switchTab(isWork) {
+    setWorking(isWork);
+    AsyncStorage.setItem(STORAGE_TAB_KEY, isWork ? "true" : "false").then();
+  }
+  function loadTabInfo() {
+    AsyncStorage.getItem(STORAGE_TAB_KEY)
+        .then(r => r === "true" ? setWorking(true) : setWorking(false))
+        .catch(e => console.log(`[ERROR] loadTabInfo : ${e}`))
+    ;
+  }
+
+  const onChangeText = (payload) => setText(payload);
   async function addTodo() {
     if (text === "") {
       return;
@@ -30,12 +49,14 @@ export default function App() {
       ...toDos,
       [Date.now()]: {
         "text": text,
-        "working": working
+        "working": working,
+        "isDone" : false,
       }
     };
     setToDos(newTodos);
     await saveToDos(newTodos);
-    console.log(toDos);
+    // clear text input
+    textInput.current.clear();
   }
 
   async function saveToDos(item) {
@@ -71,6 +92,15 @@ export default function App() {
     );
   }
 
+  function switchDone(id) {
+    const newTodos = {...toDos};
+    const isDone = newTodos[id].isDone;
+    newTodos[id].isDone = !isDone;
+    setToDos(newTodos);
+    saveToDos(newTodos).then();
+    console.log(newTodos);
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -82,29 +112,42 @@ export default function App() {
           <Text style={{...styles.headerBtn, color: !working ? "white" : theme.grey}}>Travel</Text>
         </TouchableOpacity>
       </View>
-      <View>
-        <TextInput
-            keyboardType="default"
-            returnKeyType="done"
-            onChangeText={onChangeText}
-            onSubmitEditing={addTodo}
-            placeholder={working ? "Add a To Do!" : "Where do you want to go?"}
-            style={styles.input}
-        />
-        <ScrollView>
-          {Object.keys(toDos).map(key => (
-              toDos[key].working === working ? (
-                  <View key={key} style={styles.toDo}>
-                    <Text style={styles.toDoText}>{toDos[key].text}</Text>
-                    <TouchableOpacity onPress={() => deleteTodo(key)}>
-                      <Fontisto name="trash" size={15} color={theme.grey} />
+      <TextInput
+          ref={textInput}
+          keyboardType="default"
+          returnKeyType="done"
+          onChangeText={onChangeText}
+          onSubmitEditing={addTodo}
+          placeholder={working ? "Add a To Do!" : "Where do you want to go?"}
+          style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map(key => (
+            toDos[key].working === working ? (
+                <View key={key} style={styles.toDo}>
+                  <View style={{flexDirection: "row", alignItems: "center"}} >
+                    <TouchableOpacity onPress={() => switchDone(key)}>
+                      {toDos[key].isDone ? (
+                        <Fontisto name={"checkbox-active"} size={15} color="white" />
+                      ) : (
+                        <Fontisto name={"checkbox-passive"} size={15} color="white" />
+                      )}
                     </TouchableOpacity>
+                    <Text style={{
+                      ...styles.toDoText,
+                      color : toDos[key].isDone ? theme.grey : "white",
+                      textDecorationLine: toDos[key].isDone ? "line-through" : "none",
+                    }}>
+                      {toDos[key].text}
+                    </Text>
                   </View>
-              ) : null
-          ))}
-        </ScrollView>
-      </View>
-      <View></View>
+                  <TouchableOpacity onPress={() => deleteTodo(key)}>
+                    <Fontisto name="trash" size={15} color={theme.grey} />
+                  </TouchableOpacity>
+                </View>
+            ) : null
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -144,7 +187,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   toDoText: {
-    color: "white",
     fontSize: 16,
+    marginLeft: 10,
   },
 });

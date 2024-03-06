71dc613a77146f0b0837a3a960eeaac2355606df
Bluewind <bluewind@kakao.com>
2024-03-06 16:51:41 +0900

    [스프링 핵심 원리 - 고급편] 10강

--- inflearn/spring_core_advanced/advanced/src/main/java/hello/advanced/app/v1/OrderServiceV1.java
+++ inflearn/spring_core_advanced/advanced/src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -1,6 +1,8 @@
 package hello.advanced.app.v1;
 
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +11,18 @@ import org.springframework.stereotype.Service;
 public class OrderServiceV1 {
 
     private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
 
     public void orderItem(String itemId) throws IllegalAccessException {
-        orderRepository.save(itemId);
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderServiceV2.orderItem()");
+            orderRepository.save(itemId); // 기존 로직
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
     }
 }
--- inflearn/spring_core_advanced/advanced/src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
+++ inflearn/spring_core_advanced/advanced/src/main/java/hello/advanced/app/v2/OrderRepositoryV2.java
@@ -1,23 +1,24 @@
-package hello.advanced.app.v1;
+package hello.advanced.app.v2;
 
 
+import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 @RequiredArgsConstructor
-public class OrderRepositoryV1 {
+public class OrderRepositoryV2 {
 
-    private final HelloTraceV1 trace;
+    private final HelloTraceV2 trace;
 
-    public void save(String itemId) throws IllegalAccessException {
+    public void save(String itemId, TraceId traceId) throws IllegalAccessException {
         TraceStatus status = null;
 
         try {
-            status = trace.begin("OrderRepositoryV2.save()");
+            status = trace.beginSync(traceId, "OrderRepositoryV2.save()");
             // 저장 로직
             if (itemId.equals("ex")) {
                 throw new IllegalAccessException("예외 발생");

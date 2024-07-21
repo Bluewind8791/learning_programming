2e59b5504717981ff665c119f0e80f8c463c7706
Bluewind8791 <bluewind@kakao.com>
2024-07-21 20:52:33 +0900

    섹션 2 - 7강

---
+++ inflearn/spring_mvc_one/hello_servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회]");
+        request.getParameterNames().asIterator().forEachRemaining(name -> System.out.println(name + " = " + request.getParameter(name)));
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for (String username : usernames) {
+            System.out.println("username = " + username);
+        }
+        System.out.println();
+
+        response.getWriter().write("ok");
+    }
+}

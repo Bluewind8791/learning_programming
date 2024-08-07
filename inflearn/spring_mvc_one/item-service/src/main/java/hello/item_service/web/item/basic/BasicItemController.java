94b636a0c767c5dd059059ebab17ae17c5ed7134
Bluewind8791 <bluewind@kakao.com>
2024-08-07 22:14:37 +0900

    8/7

--- inflearn/spring_mvc_one/item-service/src/main/java/hello/item_service/web/item/basic/BasicItemController.java
+++ inflearn/spring_mvc_one/item-service/src/main/java/hello/item_service/web/item/basic/BasicItemController.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,6 +27,14 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @PostMapping("/add")
+    public String addItem(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId()); // redirect path variable 로 치환된다
+        redirectAttributes.addAttribute("status", true); // 쿼리파라미터로 넘어간다
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
